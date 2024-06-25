ace60f435381c75af62b6ad7ed4a0f7c2e4ebe9d
ZinThantWin <zinthantwin1997@gmail.com>
2024-06-25 10:29:20 +0630

    Initial Commit

---
+++ SimpleMap/SimpleMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleMapApp.swift
+//  SimpleMap
+//
+//  Created by ကင်ဇို on 25/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct SimpleMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
